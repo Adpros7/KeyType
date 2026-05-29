55a8b72adcb8b6658fd2d76801f2e1f5a375d454
johnbean393 <johnbean393@gmail.com>
2026-05-29 13:24:58 +0800

    Initial Commit

---
+++ KeyTypeTests/KeyTypeTests.swift
@@ -0,0 +1,19 @@
+//
+//  KeyTypeTests.swift
+//  KeyTypeTests
+//
+//  Created by John Bean on 5/29/26.
+//
+
+import Testing
+@testable import KeyType
+
+struct KeyTypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
